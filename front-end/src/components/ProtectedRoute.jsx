c542ff694688ba4a725653cea7be7c922bbd38c6
Admin <135419938+nguyennguyennam@users.noreply.github.com>
2025-11-16 09:58:50 +0700

    update back-end cli

--- front-end/src/components/ProtectedRoute.jsx
+++ front-end/src/components/ProtectedRoute.jsx
@@ -2,10 +2,22 @@ import { Navigate } from "react-router-dom";
 import { useUser } from "../auth/useAuth";
 
 export default function ProtectedRoute({ children }) {
+  const accessToken = localStorage.getItem("accessToken");
+  const userCached = localStorage.getItem("user");
+
+  const hasAuth = !!accessToken || !!userCached;
+
+  if (!hasAuth) {
+    return <Navigate to="/login" replace />;
+  }
+
   const { data, isError, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
-  if (isError || !data ) return <Navigate to="/login" replace />;
+
+  if (isError || !data) {
+    return <Navigate to="/login" replace />;
+  }
 
   return children;
 }
